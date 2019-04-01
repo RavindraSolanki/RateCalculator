3e978647085da31f3734c2f3faac2306722bcd7d
Ravindrakumar Solanki <ravindrakumars@interxion.com>
2019-04-01 09:36:53 +0100

    Add implementation for loading CSV lender file

---
+++ src/LoanServices/Utils/LoadLenders.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RateCalculator.LoanServices.Utils
+{
+    public static class LoadLenders
+    {
+        public static IEnumerable<Lender> GetFromCsvFile(string csvFilePath)
+        {
+            var lenders = from line in File.ReadAllLines(csvFilePath).Skip(1)
+                let columns = line.Split(',')
+                select new Lender
+                {
+                    Name = columns[0],
+                    Rate = decimal.Parse(columns[1]),
+                    AvailableAmount = decimal.Parse(columns[2])
+                };
+
+            return lenders;
+        }
+    }
+}
